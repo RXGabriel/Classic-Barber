bf7081d984b3a514335990a767edadf1b02664dd
RXGabriel <gabriel2005rf@gmail.com>
2024-08-09 12:03:19 -0300

    feat: add Search and quick search options to Home component

--- app/page.tsx
+++ app/page.tsx
@@ -2,8 +2,9 @@ import React from "react"
 import { Button } from "./_components/ui/button"
 import Header from "./_components/header"
 import Image from "next/image"
-import { SearchIcon } from "lucide-react"
-import { Input } from "./_components/ui/input"
+import Search from "./_components/search"
+import { quickSearchOptions } from "./_constants/search"
+import Link from "next/link"
 
 const Home = () => {
   return (
@@ -13,11 +14,39 @@ const Home = () => {
         <h2 className="text-xl font-bold">Olá, Convidado!</h2>
         <p>Segunda-feira, 05 de dezembro</p>
 
-        <div className="mt-6 flex items-center gap-2">
-          <Input placeholder="Faça sua busca..." />
-          <Button>
-            <SearchIcon />
-          </Button>
+        <div className="mt-6">
+          <Search />
+        </div>
+
+        <div className="mt-6 flex gap-3 overflow-x-scroll [&::-webkit-scrollbar]:hidden">
+          {quickSearchOptions.map((option) => (
+            <Button
+              className="gap-2"
+              variant="secondary"
+              key={option.title}
+              asChild
+            >
+              <Link href={`/barbershops?service=${option.title}`}>
+                <Image
+                  src={option.imageUrl}
+                  width={16}
+                  height={16}
+                  alt={option.title}
+                />
+                {option.title}
+              </Link>
+            </Button>
+          ))}
+        </div>
+
+        {/* IMAGEM */}
+        <div className="relative mt-6 h-[150px] w-full">
+          <Image
+            alt="Agende nos melhores com FSW Barber"
+            src="/banner-01.png"
+            fill
+            className="rounded-xl object-cover"
+          />
         </div>
 
         <div className="relative mt-6 h-[150px] w-full">
