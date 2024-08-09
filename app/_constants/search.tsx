63656da4d66e4285cb3cf56cd024d03181622fa7
RXGabriel <gabriel2005rf@gmail.com>
2024-08-09 11:50:04 -0300

    feat: add quick search options with images and titles

---
+++ app/_constants/search.tsx
@@ -0,0 +1,31 @@
+interface QuickSearchOption {
+  imageUrl: string
+  title: string
+}
+
+export const quickSearchOptions: QuickSearchOption[] = [
+  {
+    imageUrl: "/public/cabelo.svg",
+    title: "Cabelo",
+  },
+  {
+    imageUrl: "/public/barba.svg",
+    title: "Barba",
+  },
+  {
+    imageUrl: "/public/acabamento.svg",
+    title: "Acabamento",
+  },
+  {
+    imageUrl: "/public/massagem.svg",
+    title: "Massagem",
+  },
+  {
+    imageUrl: "/public/sobrancelha.svg",
+    title: "Sobrancelha",
+  },
+  {
+    imageUrl: "/public/hidratacao.svg",
+    title: "Hidratação",
+  },
+]
